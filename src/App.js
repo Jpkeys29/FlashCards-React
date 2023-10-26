96c687f79db8572e9348329d3100d62febd22460
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-26 07:15:55 -0400

    Project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div>
+      <FlashCards />
+    </div>
+  );
+}
+
+function FlashCards(){
+  return <div>TODO</div>
+}
