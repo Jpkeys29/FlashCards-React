00bdcbafcdc4351df0b7536f07a11fe4fc735ac1
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-26 07:24:45 -0400

    Added styles and array of objects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 import "./index.css";
+import "./styles.css"
 
 export default function App() {
   return (
@@ -9,6 +10,40 @@ export default function App() {
   );
 }
 
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript"
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components"
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX"
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props"
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook"
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element"
+  }
+];
+
 function FlashCards(){
-  return <div>TODO</div>
+  return <div>Vamos con Toda!</div>
 }
